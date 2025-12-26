3de2b318a3cc9df3e95f521c772ec88a7611ed4f
Norman Walsh <ndw@nwalsh.com>
2025-12-26 16:21:32 +0000

    Avoid non-XML characters in failure documents
    Fix #79
    
    When a non-XML character causes a parse to fail, its representation in the
    failure document is "0xNNNN" where "NNNN" is its codepoint. That’s not a
    standard representation, but "&#xNNNN;" makes the failure document unparseable.

--- coffeefilter/src/test/java/org/nineml/coffeefilter/ParserFailTest.java
+++ coffeefilter/src/test/java/org/nineml/coffeefilter/ParserFailTest.java
@@ -49,4 +49,48 @@ public class ParserFailTest {
             System.err.println(ex.getMessage());
         }
     }
+
+    @Test
+    public void nonXmlChars() {
+        String input = "date: s?, day, -s, month, (-s, year)? .\n" +
+                "-s: -\" \"+ .\n" +
+                "day: digit, digit? .\n" +
+                "-digit: \"0\"; \"1\"; \"2\"; \"3\"; \"4\"; \"5\"; \"6\"; \"7\"; \"8\"; \"9\".\n" +
+                "month: \"January\"; \"February\"; \"March\"; \"April\";\n" +
+                "       \"May\"; \"June\"; \"July\"; \"August\";\n" +
+                "       \"September\"; \"October\"; \"November\"; \"December\".\n" +
+                "year: ((digit, digit); -\"'\")?, digit, digit .";
+
+        InvisibleXmlParser parser = invisibleXml.getParserFromIxml(input);
+
+        input = "16 \u0001 1992";
+        InvisibleXmlDocument doc = parser.parse(input);
+
+        Processor processor = new Processor(false);
+        DocumentBuilder builder = processor.newDocumentBuilder();
+
+        try {
+            BuildingContentHandler bch = builder.newBuildingContentHandler();
+            doc.getTree(bch);
+            XdmNode node = bch.getDocumentNode();
+
+            String str = node.toString();
+            if (doc.getParserType() == ParserType.Earley) {
+                Assertions.assertTrue(str.contains("<line>1</line>"));
+                Assertions.assertTrue(str.contains("<column>4</column>"));
+                Assertions.assertTrue(str.contains("<pos>4</pos>"));
+                Assertions.assertTrue(str.contains(" <unexpected codepoint=\"#0001\">U+0001</unexpected>"));
+                Assertions.assertTrue(str.contains("<permitted>' ', 'A', 'D', 'F', 'J', 'M', 'N', 'O', 'S'</permitted>"));
+            }
+            if (doc.getParserType() == ParserType.GLL) {
+                Assertions.assertTrue(str.contains("<line>1</line>"));
+                Assertions.assertTrue(str.contains("<column>4</column>"));
+                Assertions.assertTrue(str.contains("<pos>4</pos>"));
+                Assertions.assertTrue(str.contains(" <unexpected codepoint=\"#0001\">U+0001</unexpected>"));
+            }
+        } catch (SaxonApiException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
